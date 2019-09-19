f438607e350c32f105d803f77bf509394fb341da
Anonymous <anonymous@tor.com>
2019-09-19 14:59:18 +0800

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,12 @@
-import styles from './index.css';
+import React from 'react'
+import styles from 'index.less'
 
-export default function() {
-  return (
-    <div className={styles.normal}>
-      <div className={styles.welcome} />
-      <ul className={styles.list}>
-        <li>To get started, edit <code>src/pages/index.js</code> and save to reload.</li>
-        <li>
-          <a href="https://umijs.org/guide/getting-started.html">
-            Getting Started
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+export default class Index extends React.Component {
+  render () {
+    return (
+      <div>
+        hi, utools
+      </div>
+    )
+  }
 }
